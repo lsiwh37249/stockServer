ad3723dcb5852f49a39b0f776007e3e7af69ddf3
kim <lsiwh32749@gmail.com>
2025-01-18 14:26:53 +0900

    init

---
+++ src/test/java/stockserver/StockProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package stockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello World");
+	}
+
+}
