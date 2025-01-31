b2cbfb45842d636143b599d61d50bd87fee8c6d1
kim <lsiwh32749@gmail.com>
2025-02-01 01:33:50 +0900

    add : token 예외 처리 추가

--- src/main/java/stockserver/oauth/service/OauthService.java
+++ src/main/java/stockserver/oauth/service/OauthService.java
@@ -10,22 +10,27 @@ import stockserver.oauth.dto.TokenRequestDto;
 import stockserver.oauth.dto.WebSocketRequestDto;
 import stockserver.redis.RedisService;
 
+import java.time.LocalDateTime;
+
 @Service
-public class OauthService{
+public class
+OauthService{
 
     private final WebClient webClient;
     private final WebSocketRequestDto requestDto;
     private final TokenRequestDto tokenRequestDto;
     private final RedisService redisService;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public OauthService(WebSocketRequestDto requestDto, TokenRequestDto tokenRequestDto, RedisService redisService) {
+    public OauthService(WebSocketRequestDto requestDto, TokenRequestDto tokenRequestDto, RedisService redisService, ObjectMapper objectMapper) {
         this.redisService = redisService;
         this.webClient = WebClient.builder()
                 .baseUrl("https://openapi.koreainvestment.com:9443")
                 .build();
         this.requestDto = requestDto;
         this.tokenRequestDto = tokenRequestDto;
+        this.objectMapper = objectMapper;
     }
 
     // 실시간 데이터
@@ -48,14 +53,7 @@ public class OauthService{
         // value 추출
         return jsonNode.get("approval_key").asText();
     }
-
-    public String getToken() throws JsonProcessingException{
-        TokenRequestDto tokenRequestDto = this.tokenRequestDto;
-        RedisService redisService = this.redisService;
-        System.out.println(tokenRequestDto.toJson());
-
-        //토큰 안에 만료 시각이 지금보다 늦다? 그리면 다시 요청하고 그렇지 않으면 accessToken값 요처
-
+    public String requestToken() throws JsonProcessingException {
         String response = String.valueOf(webClient.post()
                 .uri("/oauth2/tokenP") // 요청 URI
                 .header("content-type" , "application/json")
@@ -65,14 +63,36 @@ public class OauthService{
                 .block()); // 비동기 결과를 동기로 변환 (주의: 블록킹)
 
         System.out.println("POST Response: " + response);
-        // ObjectMapper 생성
-        ObjectMapper objectMapper = new ObjectMapper();
         // JSON 파싱
         JsonNode jsonNode = objectMapper.readTree(response);
 
         //redis에 저장하기
-
+        redisService.insert("tokenInfo", String.valueOf(jsonNode));
+        System.out.println(redisService.read("tokenInfo"));
 
         return jsonNode.get("token_type").asText() + " " + jsonNode.get("access_token").asText();
+    }
+
+    public String getToken() throws JsonProcessingException {
+        String token = redisService.read("tokenInfo");
+
+        if (token == null) {
+            return requestToken();
+        }
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(token);
+            LocalDateTime tokenExpired = LocalDateTime.parse(jsonNode.get("access_token_token_expired").asText());
+            System.out.println(tokenExpired);
+            if (tokenExpired.isAfter(LocalDateTime.now().plusDays(1))) {
+                return jsonNode.get("token_type").asText() + " " + jsonNode.get("access_token").asText();
+            }
+            else {
+                return requestToken();
+            }
+        }
+        catch(JsonProcessingException e) {
+            return requestToken();
+        }
     }
 }
