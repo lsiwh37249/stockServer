3871c24e2055c45ad305f937437093b929c271ac
kim <lsiwh32749@gmail.com>
2025-02-01 01:52:00 +0900

    refact : WebClient 싱글톤으로 변경

---
+++ src/main/java/stockserver/webclient/WebClientConfig.java
@@ -0,0 +1,24 @@
+package stockserver.webclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .baseUrl("https://openapi.koreainvestment.com:9443")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
